5082132300ae58fd764b2c2a167745eeabb1fe26
Natanael Mauro Fiorilla <nfiorilla@ohpen.com>
2021-12-06 17:31:44 +0100

    push

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import * as core from '@actions/core'
+import * as event from './event'
+import * as version from './version'
 
 export async function run(): Promise<void> {
   try {
+    const tag = event.getCreatedTag()
+
+    if (tag && version.isSemVer(tag)) {
+      //TODO
+    }
+
     core.setOutput('release-url', 'https://google.com')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)

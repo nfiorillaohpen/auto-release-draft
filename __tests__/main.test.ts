9ef2fafd3c35a95949d45f1fb233ac9dbd5adb15
Natanael Mauro Fiorilla <nfiorilla@ohpen.com>
2021-12-06 17:12:28 +0100

    push

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,13 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import * as core from '@actions/core'
+import { run } from '../src/main'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
+jest.mock('@actions/core')
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+describe('When running the action', () => {
+    const fakeSetOutput = core.setOutput as jest.MockedFunction<typeof core.setOutput>
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+    test('it shlould set the release-url output parameter', async () => {
+        await run()
+        expect(fakeSetOutput).toHaveBeenCalledWith('release-url', expect.anything())
+    })  
 })
